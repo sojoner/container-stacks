e2dee37e3c15a83edcc29199e04c3a758b4ea04d
Sojoner <hagen.toennies@googlemail.com>
2017-01-24 16:19:58 +0100

    Add fake producer

--- clj-kstream/up.sh
+++ clj-kstream/up.sh
@@ -19,16 +19,15 @@ echo ">> Please 'source env.sh' to pick up the IP adress of the Consul node"
 
 echo "start clj-kstream-cutter"
 docker-compose up -d clj-kstream-cutter
-sleep 5
+sleep 1
 
 echo "start clj-kstream-hh"
 docker-compose up -d clj-kstream-hh
-sleep 5
+sleep 1
 
 echo "start clj-kstream-string-long-window-aggregate"
 docker-compose up -d clj-kstream-string-long-window-aggregate
-sleep 5
+sleep 1
 
 echo "start clj-kstream-elasticsearch-sink"
 docker-compose up -d clj-kstream-elasticsearch-sink
-sleep 5
