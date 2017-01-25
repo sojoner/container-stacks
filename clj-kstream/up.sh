8897332ae493f0ce1d16927ffd705fd5e5572ca9
Sojoner <hagen.toennies@googlemail.com>
2017-01-25 22:35:26 +0100

    WIP

--- clj-kstream/up.sh
+++ clj-kstream/up.sh
@@ -19,15 +19,15 @@ echo ">> Please 'source env.sh' to pick up the IP adress of the Consul node"
 
 echo "start clj-kstream-cutter"
 docker-compose up -d clj-kstream-cutter
-sleep 1
+sleep 2
 
 echo "start clj-kstream-hh"
-docker-compose up -d clj-kstream-hh
-sleep 1
+#docker-compose up -d clj-kstream-hh
+sleep 2
 
 echo "start clj-kstream-string-long-window-aggregate"
-docker-compose up -d clj-kstream-string-long-window-aggregate
-sleep 1
+#docker-compose up -d clj-kstream-string-long-window-aggregate
+sleep 2
 
 echo "start clj-kstream-elasticsearch-sink"
 docker-compose up -d clj-kstream-elasticsearch-sink
