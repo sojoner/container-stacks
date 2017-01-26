4f4c5475941d776203589701b02c4d66309b1403
Sojoner <hagen.toennies@googlemail.com>
2017-01-26 17:18:54 +0100

    Split k-streams from storage systems

--- clj-kstream/up.sh
+++ clj-kstream/up.sh
@@ -11,23 +11,10 @@ if [ ! -z "${CONSUL_IP}" ]; then
 	sed -i '' -e "s/CONSUL_CLUSTER_IPS=.*/CONSUL_CLUSTER_IPS=${CONSUL_IP}/" base.yml
 fi
 echo "export CONSUL_INT_IP=${CONSUL_INT_IP}" > env.sh
-source env.sh
+
 echo "start kafka elasticsearch stack"
 docker-compose up -d zookeeper kafka-broker kafka-manager elasticsearch kibana
 sleep 5
 echo ">> Please 'source env.sh' to pick up the IP adress of the Consul node"
 
-echo "start clj-kstream-cutter"
-docker-compose up -d clj-kstream-cutter
-sleep 2
 
-echo "start clj-kstream-hh"
-#docker-compose up -d clj-kstream-hh
-sleep 2
-
-echo "start clj-kstream-string-long-window-aggregate"
-#docker-compose up -d clj-kstream-string-long-window-aggregate
-sleep 2
-
-echo "start clj-kstream-elasticsearch-sink"
-docker-compose up -d clj-kstream-elasticsearch-sink
