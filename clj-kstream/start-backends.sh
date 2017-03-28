bd2dbee951e7775c6161a5f2b884db5a0a1979aa
Christian Kniep <christian@qnib.org>
2017-03-28 11:31:10 +0200

    rm kibana, CONSUL_IP is not declared anyware

--- clj-kstream/start-backends.sh
+++ clj-kstream/start-backends.sh
@@ -6,14 +6,10 @@ docker-compose up -d consul
 
 ## Get IP
 CONSUL_INT_IP=$(docker inspect -f '{{ .NetworkSettings.Networks.cljkstream_network.IPAddress }}' consul)
-if [ ! -z "${CONSUL_IP}" ]; then
-	sed -i '' -e "s/dns: .*/dns: ${CONSUL_IP}/" base.yml
-	sed -i '' -e "s/CONSUL_CLUSTER_IPS=.*/CONSUL_CLUSTER_IPS=${CONSUL_IP}/" base.yml
-fi
 echo "export CONSUL_INT_IP=${CONSUL_INT_IP}" > env.sh
 
 echo "start kafka elasticsearch stack"
-docker-compose up -d zookeeper kafka-broker kafka-manager elasticsearch kibana
+docker-compose up -d zookeeper kafka-broker kafka-manager elasticsearch
 sleep 5
 echo ">> Please 'source env.sh' to pick up the IP adress of the Consul node"
 
